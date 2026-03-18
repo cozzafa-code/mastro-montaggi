a0ea63b329058114599ddad0dd451aa4b3836b53
Fabio Cozza <cozzafa@gmail.com>
2026-03-19 00:46:20 +0100

    fix: rename import dynamic -> loadDynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import dynamic from 'next/dynamic'
+import loadDynamic from 'next/dynamic'
 
 export const dynamic = 'force-dynamic'
 
-const MastroMontaggiApp = dynamic(
+const MastroMontaggiApp = loadDynamic(
   () => import('@/components/MastroMontaggiApp'),
   { ssr: false }
 )
