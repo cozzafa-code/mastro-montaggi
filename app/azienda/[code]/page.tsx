acf31e1ccfcf35591d1243d2333b0c727b823a9b
cozzafa-code <cozzafa@gmail.com>
2026-04-08 17:00:24 +0200

    fix: remove inviteCode prop from PortaleAzienda

--- app/azienda/[code]/page.tsx
+++ app/azienda/[code]/page.tsx
@@ -1,6 +1,6 @@
-// app/azienda/[code]/page.tsx
-import PortaleAzienda from '../../../components/PortaleAzienda';
+"use client";
+import PortaleAzienda from "@/components/PortaleAzienda";
 
 export default function AziendaPage({ params }: { params: { code: string } }) {
-  return <PortaleAzienda inviteCode={params.code} />;
+  return <PortaleAzienda />;
 }
